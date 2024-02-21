9b8b76fc0e5ce97ee4a88185db3a544bc9a64511
Matias Alicata <matiasalicata@gmail.com>
2024-02-21 13:35:40 -0300

    add changes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.use('/api', authRoute);
 app.use('/api' , ventaRoute);
